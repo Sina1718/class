d3d71458c8c880441ff6f661f343d631fb737706
Sina1718 <119540069+Sina1718@users.noreply.github.com>
2022-12-28 20:35:14 +0300

    Add files via upload

---
+++ project class student.py
@@ -0,0 +1,13 @@
+class Student:
+  def __init__(self, name, age , reshte , jensiat , studentID , city):
+    self.name = name
+    self.age = age
+    self.reshte = reshte
+    self.jensiat = jensiat
+    self.studentID = studentID
+    self.city = city
+
+p1 = Person("John", 36 , "IT" , "Male" , 123412 , "karaj")
+
+print(p1.name)
+print(p1.age)
